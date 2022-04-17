2ea1276b298e63f4f54f4a746373f86985603426
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2022-04-17 07:25:47 +0700

    Add project files.

---
+++ BervProject.FeatureFlag.Demo/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Amazon.AppConfigData;
+using BervProject.FeatureFlag;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((_, services) =>
+    {
+        services.AddAWSService<IAmazonAppConfigData>();
+        services.AddSingleton<FeatureFlag>();
+    })
+    .Build();
+
+CheckFeatureFlag(host.Services);
+
+await host.RunAsync();
+
+static void CheckFeatureFlag(IServiceProvider services)
+{
+    using IServiceScope serviceScope = services.CreateScope();
+    IServiceProvider provider = serviceScope.ServiceProvider;
+
+    var featureFlag = provider.GetRequiredService<FeatureFlag>();
+    var ffTask = featureFlag.GetFeatureFlag("try_feature_flag");
+    ffTask.Wait();
+    var featureFlagResult = ffTask.Result;
+    Console.WriteLine($"try_feature_flag result: {featureFlagResult}");
+}
---
+++ FeatureFlag/FeatureFlag.cs
@@ -0,0 +1,52 @@
+﻿using Amazon.AppConfigData;
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BervProject.FeatureFlag
+{
+    public class FeatureFlag
+    {
+        private readonly IAmazonAppConfigData _amazonAppConfigDataClient;
+        private string _token;
+        public FeatureFlag(IAmazonAppConfigData amazonAppConfigData)
+        {
+            _amazonAppConfigDataClient = amazonAppConfigData;
+        }
+        public async Task<bool> GetFeatureFlag(string feature)
+        {
+            if (string.IsNullOrEmpty(_token))
+            {
+                var response = await _amazonAppConfigDataClient.StartConfigurationSessionAsync(new Amazon.AppConfigData.Model.StartConfigurationSessionRequest
+                {
+                    ApplicationIdentifier = Environment.GetEnvironmentVariable("APPCONFIG_APPLICATION_IDENTIFIER"),
+                    ConfigurationProfileIdentifier = Environment.GetEnvironmentVariable("APPCONFIG_CONFIGURATION_PROFILE_IDENTIFIER"),
+                    EnvironmentIdentifier = Environment.GetEnvironmentVariable("APPCONFIG_ENVIRONMENT_IDENTIFIER")
+                });
+                _token = response.InitialConfigurationToken;
+            }
+            var latestConfig = await _amazonAppConfigDataClient.GetLatestConfigurationAsync(new Amazon.AppConfigData.Model.GetLatestConfigurationRequest
+            {
+                ConfigurationToken = _token
+            });
+            if (latestConfig.ContentLength > 0)
+            {
+                using (var jsonDocument = JsonDocument.Parse(latestConfig.Configuration))
+                {
+                    if (jsonDocument.RootElement.ValueKind == JsonValueKind.Object)
+                    {
+                        return jsonDocument.RootElement.GetProperty(feature).GetBoolean();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
